38a1e0e45b752d4c1f0fe3bbc235da6e7749e42f
Maqammed23 <mehemmed23062012@gmail.com>
2025-12-19 23:35:03 +0400

    eTGRFDS

---
+++ menyu.js
@@ -0,0 +1,20 @@
+function mcCrispy() {
+  let say = prompt("Neçə ədəd Mc Crispy istəyirsən?");
+  if (say) {
+    alert(say + " ədəd Mc Crispy sifariş olundu 🍔");
+  }
+}
+
+function quarterPounder() {
+  let say = prompt("Neçə ədəd Quarter Pounder istəyirsən?");
+  if (say) {
+    alert(say + " ədəd Quarter Pounder sifariş olundu 🍔");
+  }
+}
+
+function snackWrap() {
+  let say = prompt("Neçə ədəd Snack Wrap istəyirsən?");
+  if (say) {
+    alert(say + " ədəd Snack Wrap sifariş olundu 🌯");
+  }
+}
