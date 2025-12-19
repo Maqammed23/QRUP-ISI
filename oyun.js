4fb01f55b30f2552791fd64889fdb5827229b410
ismayil61 <ismayilsahane@gmail.com>
2025-12-19 16:45:09 +0400

    html

--- oyun.js
+++ oyun.js
@@ -1,47 +1,34 @@
 function Quiz() {
+  let dogru = 0;
+  let sual1 = prompt(
+    "Bu işarələmə dili vasitəsi ilə web səhifəmizin strukturunu qururuq."
+  );
+  if (sual1 && sual1.toUpperCase() === "HTML") {
+    dogru++;
+  }
 
-    let span = document.querySelector("span");
+  let sual2 = prompt(
+    "Bu işarələmə dili vasitəsi ilə web səhifəmizin dizaynını qururuq."
+  );
+  if (sual2 && sual2.toUpperCase() === "CSS") {
+    dogru++;
+  }
+  let sual3 = prompt(
+    "Bu proqramlaşdırma dili vasitəsi ilə web səhifəmizin funksionallığını qururuq."
+  );
+  if (sual3 && sual3.toUpperCase() === "JS") {
+    dogru++;
+  }
+  document.getElementById("result").innerText =
+    "Düzgün cavabların sayı: " + dogru;
 
-    let sualCavab = [
-        {
-            sual: "",
-            cavab: "HTML"
-        },
-        {
-            sual: "Saytın dizaynını qurur",
-            cavab: "CSS"
-        },
-        {
-            sual: "Saytın funksionallığını qurur",
-            cavab: "JS"
-        }
-    ];
-
-    let duzgunCavabSayi = 0;
-
-    sualCavab.forEach(element => {
-        let promptSual = prompt(element.sual);
-        if (promptSual == element.cavab) {
-            duzgunCavabSayi++;
-        }
-    });
-
-    span.innerText = duzgunCavabSayi;
-
-    switch (duzgunCavabSayi) {
-        case 0:
-            document.body.style.backgroundColor = "red";
-            break;
-        case 1:
-            document.body.style.backgroundColor = "yellow";
-            break;
-        case 2:
-            document.body.style.backgroundColor = "blue";
-            break;
-        case 3:
-            document.body.style.backgroundColor = "green";
-            break;
-        default:
-            break;
-    }
+  if (dogru === 0) {
+    document.body.style.backgroundColor = "red";
+  } else if (dogru === 1) {
+    document.body.style.backgroundColor = "yellow";
+  } else if (dogru === 2) {
+    document.body.style.backgroundColor = "blue";
+  } else if (dogru === 3) {
+    document.body.style.backgroundColor = "green";
+  }
 }
